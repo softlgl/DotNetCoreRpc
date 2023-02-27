45b52c0a63ffdab703ca3aa4fa1aff5fa06ffb8f
liguoliang <liguoliang@chinalawinfo.com>
2023-02-27 16:21:32 +0800

    序列化设置扩展

--- src/DotNetCoreRpc.Core/JsonExtensions.cs
+++ src/DotNetCoreRpc.Core/JsonExtensions.cs
@@ -13,39 +13,39 @@ namespace DotNetCoreRpc.Core
             ReferenceHandler = ReferenceHandler.IgnoreCycles
         };
 
-        public static string ToJson<T>(this T data) where T:class,new()
+        public static string ToJson<T>(this T data, JsonSerializerOptions options = default) where T:class,new()
         {
-            return JsonSerializer.Serialize(data, serializerOptions);
+            return JsonSerializer.Serialize(data, options ?? serializerOptions);
         }
 
-        public static T FromJson<T>(this string json) where T : class, new()
+        public static T FromJson<T>(this string json, JsonSerializerOptions options = default) where T : class, new()
         {
-            return JsonSerializer.Deserialize<T>(json, serializerOptions);
+            return JsonSerializer.Deserialize<T>(json, options ?? serializerOptions);
         }
 
-        public static object FromJson(this string json,Type type)
+        public static object FromJson(this string json, Type type, JsonSerializerOptions options = default)
         {
-            return JsonSerializer.Deserialize(json, type, serializerOptions);
+            return JsonSerializer.Deserialize(json, type, options ?? serializerOptions);
         }
 
-        public static T FromJson<T>(this byte[] utf8Json) where T : class, new()
+        public static T FromJson<T>(this byte[] utf8Json, JsonSerializerOptions options = default) where T : class, new()
         {
-            return JsonSerializer.Deserialize<T>(utf8Json, serializerOptions);
+            return JsonSerializer.Deserialize<T>(utf8Json, options ?? serializerOptions);
         }
 
-        public static object FromJson(this byte[] utf8Json, Type type)
+        public static object FromJson(this byte[] utf8Json, Type type, JsonSerializerOptions options = default)
         {
-            return JsonSerializer.Deserialize(utf8Json, type, serializerOptions);
+            return JsonSerializer.Deserialize(utf8Json, type, options ?? serializerOptions);
         }
 
-        public static T FromJson<T>(this JsonElement jsonElement) where T : class, new()
+        public static T FromJson<T>(this JsonElement jsonElement, JsonSerializerOptions options = default) where T : class, new()
         {
-            return jsonElement.Deserialize<T>(serializerOptions);
+            return jsonElement.Deserialize<T>(options ?? serializerOptions);
         }
 
-        public static object FromJson(this JsonElement jsonElement, Type type)
+        public static object FromJson(this JsonElement jsonElement, Type type, JsonSerializerOptions options = default)
         {
-            return jsonElement.Deserialize(type, serializerOptions);
+            return jsonElement.Deserialize(type, options ?? serializerOptions);
         }
     }
 }
